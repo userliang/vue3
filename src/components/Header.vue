a576fd42c76d3367e68e3a47764cba21ffcf04c3
liangtaojkl123 <547508229@qq.com>
2021-09-27 17:32:12 +0800

    feat: init

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="title" @click="router.push('/')">
+      Vite2.x + Vue3.x + TypeScript Starter
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+  setup() {
+    const goGitHub = () => {
+      window.open('https://github.com/XPoet/vite-vue3-starter')
+    }
+    return {
+      goGitHub
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  font-weight: bold;
+
+  .title {
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  .go-github {
+    cursor: pointer;
+    font-size: 16px;
+
+    .icon {
+      font-size: 20px;
+    }
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import router from '@/router/index'
+import { key, store } from '@/store'
+import App from './App.vue'
+import styleImport from '@/utils/style-import'
+import 'element-plus/dist/index.css'
+import '@/style/basic.scss'
+
+const app = createApp(App)
+styleImport(app).use(ElementPlus).use(router).use(store, key).mount('#app')
